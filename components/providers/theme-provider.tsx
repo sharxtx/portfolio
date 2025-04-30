5ba995e6a2b31e0486c1b23d16ba528b20feb358
Sharath2205 <pendyalasharath321@gmail.com>
2025-04-30 22:07:39 +0530

    fix: resolves theme hydration error

--- components/providers/theme-provider.tsx
+++ components/providers/theme-provider.tsx
@@ -1,14 +1,16 @@
-"use client"
+'use client'
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-// import dynamic from "next/dynamic.js"
+import * as React from 'react'
+const NextThemesProvider = dynamic(
+    () => import('next-themes').then((e) => e.ThemeProvider),
+    {
+        ssr: false,
+    }
+)
 
-// const NoSSR = dynamic(() => import('@/components/providers/theme-provider'), { ssr: false })
+import type { ThemeProviderProps } from 'next-themes'
+import dynamic from 'next/dynamic'
 
-
-export function ThemeProvider({
-    children,
-    ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
