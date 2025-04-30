cc6459df05218c4d3a347b0841c2a74b9151354d
Sharath2205 <pendyalasharath321@gmail.com>
2025-04-30 22:06:08 +0530

    added not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import Link from 'next/link';
+// import notfound from '@/media/404.png';
+import notfound1 from '@/media/notfound.png';   
+
+import { Button } from '@/components/ui/button';
+import Container from '@/components/ui/custom/Container';
+
+export default function NotFound() {
+    return (
+        <Container
+            className=" min-h-screen flex items-center justify-center relative px-4 text-center"
+            
+        >
+            <div
+                className="absolute top-0 left-0 w-[500px] h-[500px] rounded-full mix-blend-multiply filter blur-3xl animate-pulse opacity-30"
+                style={{ backgroundColor: 'var(--fancy-blob-1)' }}
+            />
+            <div
+                className="absolute bottom-0 right-0 w-[400px] h-[400px] rounded-full mix-blend-multiply filter blur-2xl animate-pulse opacity-20"
+                style={{ backgroundColor: 'var(--fancy-blob-2)' }}
+            />
+            <div className="relative flex flex-col gap-2 items-center justify-center text-center p-8">
+                <Image
+                    src={notfound1}
+                    alt="404 Not Found"
+                    width={200}
+                    height={200}
+                />
+                <h1 className="my-4 text-xl font-windsong font-bold">Oops, page was not found!</h1>
+                <p className="mb-6 max-w-md text-sm">
+                    Oops! The page you&apos;re looking for doesn&apos;t exist or has been moved.
+                </p>
+
+                <Button variant="default">
+                    <Link href="/" className=" no-underline px-4 py-2">
+                        Go Back Home
+                    </Link>
+                </Button>
+            </div>
+        </Container>
+    );
+}
