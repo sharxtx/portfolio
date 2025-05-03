128465315a92386c93d782ae00c8d3a82ff15169
Sharath2205 <pendyalasharath321@gmail.com>
2025-05-03 21:41:49 +0530

    feat: contact me

--- components/sections/home/Contact.tsx
+++ components/sections/home/Contact.tsx
@@ -1,8 +1,86 @@
+'use client'
+
+import Container from '@/components/ui/custom/Container'
 import React from 'react'
+import ContactForm from './Contact/ContactForm'
+import { Button } from '@/components/ui/button'
+import { Github } from '@/components/ui/icons/Github'
+import { X } from '@/components/ui/icons/X'
+import { Mail } from 'lucide-react'
+import { LinkedIn } from '@/components/ui/icons/Linkedin'
+import Link from 'next/link'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id="contact" className="flex flex-col items-center justify-center w-full relative overflow-hidden md:mt-24 border-t-2 pt-24 md:py-24">
+      <Container className='relative w-full flex flex-col md:flex-row md:gap-24 gap-8 items-center justify-around px-4 text-center'>
+        <section className='w-full md:w-1/2'>
+          <h2 className="text-3xl md:text-8xl md:flex flex-col justify-between gap-5 font-bold animate-gradient tracking-widest">
+            {['LET\'S ', 'GET IN ', 'TOUCH '].map((text, index) => (
+              <span key={text} className={`md:text-${index % 2 === 1 ? 'right' : 'left'} md:font-serif md:text-muted-foreground`}>{text}</span>
+            ))}
+          </h2>
+        </section>
+        <section className="w-full md:w-1/2 p-8 bg-background rounded-lg shadow-sm">
+          <div className="max-w-md mx-auto">
+            <ContactForm />
+
+            <div className="relative my-6">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+
+            <div className="flex justify-center gap-4">
+              <Link href="https://github.com/sharxtx" target="_blank">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="h-10 w-10 rounded-full hover:bg-accent transition-colors cursor-pointer"
+                >
+                  <Github className="h-5 w-5" />
+                </Button>
+              </Link>
+
+              <Link href="https://x.com/sharxtx" target="_blank">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="h-10 w-10 rounded-full hover:bg-accent transition-colors cursor-pointer"
+                >
+                  <X className="h-5 w-5" />
+                </Button>
+              </Link>
+
+              <Link href="https://www.linkedin.com/in/psharath2205/" target="_blank">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="h-10 w-10 rounded-full hover:bg-accent transition-colors cursor-pointer"
+                >
+                  <LinkedIn className="h-5 w-5" />
+                </Button>
+              </Link>
+
+              <a href="mailto:pendyalasharath321@gmail.com">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="h-10 w-10 rounded-full hover:bg-accent transition-colors cursor-pointer"
+                >
+                  <Mail className="h-5 w-5" />
+                </Button>
+              </a>
+            </div>
+          </div>
+        </section>
+      </Container>
+    </section>
   )
 }
 
---
+++ components/sections/home/Contact/ContactForm.tsx
@@ -0,0 +1,160 @@
+'use client'
+
+import { z } from "zod"
+// import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button'
+import { toast } from "sonner"
+import { Toaster } from "@/components/ui/sonner"
+import { ArrowRight } from "lucide-react"
+
+
+const contactFormSchema = z.object({
+  fullname: z.string().min(2, "Your name is less than 2 characters?").max(50, "Why so long? Try to keep it under 50 characters."),
+  email: z.string().email({message: "Not a valid email don't you think?"}),
+  message: z.string().min(10, "Your message is less than 10 characters?").max(500, "Why so long? Try to keep it under 500 characters."),
+  phone: z.string().length(10, "Not a valid Indian number.").optional(),
+})
+
+const ContactForm: React.FC = () => {
+  const form = useForm<z.infer<typeof contactFormSchema>>({
+    // resolver: zodResolver(contactFormSchema),
+    defaultValues: {
+      fullname: "",
+      email: "",
+      message: "",
+      phone: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof contactFormSchema>) {
+    toast("Still haven't implemented the mail trigger, maybe ping on other platforms?")
+    const res = contactFormSchema.safeParse(values);
+    console.log(res)
+    // if (!res.success) {
+    //   console.log(res.error.format())
+    //   toast("Yeaa, you can't do that. Maybe try filling the form correctly?")
+    //   return
+    // }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+        <FormField
+          control={form.control}
+          name="fullname"
+          render={({ field }) => (
+            <FormItem>
+              <div className="relative">
+                <FormControl>
+                  <Input
+                    autoComplete="off"
+                    className="peer bg-background px-0 pt-6 pb-2 rounded-none border-0 border-b focus-visible:ring-0 focus-visible:border-foreground"
+                    {...field}
+                  />
+                </FormControl>
+                {/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
+                <label className={`absolute tracking-widest left-0  ${form.control._formValues.fullname ? "top-0 text-xs -translate-y-2" : "top-1/2 -translate-y-1/2"}  text-muted-foreground peer-focus:top-0 peer-focus:text-xs peer-focus:-translate-y-2 transition-all duration-200 pointer-events-none`}>
+                  FULL NAME *
+                </label>
+              </div>
+            </FormItem>
+          )}
+        />
+
+        <div className="flex flex-col md:flex-row gap-8 w-full">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <div className="relative">
+                  <FormControl>
+                    <Input
+                      autoComplete="off"
+                      className="peer bg-transparent px-0 pt-6 pb-2 rounded-none border-0 border-b focus-visible:ring-0 focus-visible:border-foreground"
+                      {...field}
+                    />
+                  </FormControl>
+                  {/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
+                  <label className={`absolute tracking-widest  left-0  ${form.control._formValues.email ? "top-0 text-xs -translate-y-2" : "top-1/2 -translate-y-1/2"}  text-muted-foreground peer-focus:top-0 peer-focus:text-xs peer-focus:-translate-y-2 transition-all duration-200 pointer-events-none`}>
+                    EMAIL *
+                  </label>
+                </div>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <div className="relative">
+                  <FormControl>
+                    <Input
+                      autoComplete="off"
+                      className="peer bg-transparent/100 px-0 pt-6 pb-2 rounded-none border-0 border-b focus-visible:ring-0 focus-visible:border-foreground"
+                      {...field}
+                    />
+                  </FormControl>
+                  {/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
+                  <label className={`absolute  tracking-widest left-0  ${form.control._formValues.phone ? "top-0 text-xs -translate-y-2" : "top-1/2 -translate-y-1/2"}  text-muted-foreground peer-focus:top-0 peer-focus:text-xs peer-focus:-translate-y-2 transition-all duration-200 pointer-events-none`}>
+                    PHONE
+                  </label>
+                </div>
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <div className="relative">
+                <FormControl>
+                  <Input
+                    autoComplete="off"
+                    autoCorrect="off"
+                    className="peer bg-transparent px-0 pt-6 pb-2 rounded-none border-0 border-b focus-visible:ring-0 focus-visible:border-foreground "
+                    {...field}
+                  />
+                </FormControl>
+                {/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
+                <label className={`absolute  tracking-widest left-0  ${form.control._formValues.message ? "top-0 text-xs -translate-y-2" : "top-1/2 -translate-y-1/2"}  text-muted-foreground peer-focus:top-0 peer-focus:text-xs peer-focus:-translate-y-2 transition-all duration-200 pointer-events-none`}>
+                  MESSAGE *
+                </label>
+              </div>
+            </FormItem>
+          )}
+        />
+
+        <div className="flex items-end justify-end gap-2">
+          <Button type="submit" variant="outline" size="icon">
+            <ArrowRight className="h-4 w-4" />
+          </Button>
+        </div>
+      </form>
+
+      <Toaster
+        closeButton
+        position="bottom-right"
+        duration={3000}
+        swipeDirections={["right", "left"]}
+      />
+    </Form>
+  )
+}
+
+export default ContactForm
