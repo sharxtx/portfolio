7300f894948129e6ca0dd7d201b6332570fe16eb
Sharath2205 <pendyalasharath321@gmail.com>
2025-05-04 23:02:58 +0530

    feat: Blog section

--- components/sections/home/Blogs.tsx
+++ components/sections/home/Blogs.tsx
@@ -1,8 +1,21 @@
+import Container from '@/components/ui/custom/Container'
 import React from 'react'
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
+    <section id="blog" className="pt-16 mx-auto w-full">
+      <Container className="w-full relative">
+        <div className="mb-10 text-center">
+          <h2 className="text-4xl font-bold mb-2 tracking-widest">BLOGS</h2>
+          <p className="text-muted-foreground max-w-2xl mx-auto">
+            Logs from the Journey: Code, Systems, and Beyond.
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center text-center text-muted-foreground mt-8 h-44">
+          <p>Yeaaa, didn&apos;t write any thing till now. But I&apos;ll do it soon.</p>
+        </div>
+      </Container>
+    </section>
   )
 }
 
